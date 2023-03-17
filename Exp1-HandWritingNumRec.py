d32268dce26701d960814824159fe3454a8ce0d5
Olivia <laiyunghwei@gmail.com>
2023-03-17 15:57:21 +0800

    Update Exp1-HandWritingNumRec.py

--- Exp1-HandWritingNumRec.py
+++ Exp1-HandWritingNumRec.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 import torch.nn.functional as neural_funcs
@@ -7,16 +8,17 @@ from PIL import Image
 from numpy import asarray
 import numpy as np
 from keras.utils import np_utils
+from torch.utils.data import TensorDataset
 
 class HandWritingNumberRecognize_Dataset(Dataset):
     def __init__(self,type):
         # 这里添加数据集的初始化内容
         if type == "train":
-            self.__getitem__(0)
+            self.dataset = self.__getitem__(0)
         elif type == "val":
-            self.__getitem__(1)
+            self.dataset = self.__getitem__(1)
         elif type == "test":
-            self.__getitem__(2)
+            self.dataset = self.__getitem__(2)
 
     def __getitem__(self, index):
         # 这里添加getitem函数的相关内容
@@ -25,28 +27,34 @@ class HandWritingNumberRecognize_Dataset(Dataset):
 
         if index == 0:
             self.Y = np.loadtxt("dataset/train/labels_train.txt")
+            self.Y = torch.tensor(np.array(self.Y))
             if os.path.exists("dataset/train_X.txt"):
                 self.X = np.loadtxt("dataset/train_X.txt")
                 self.X = np.array(self.X)    #(39923, 28, 28)
                 self.X = self.X.reshape(self.X.shape[0],28,28)
-                return self.X, self.Y
+                self.X = torch.tensor(self.X)
+                return TensorDataset(self.X, self.Y)
             else:
                 path = "dataset/train/images/"
         elif index == 1:
             self.Y = np.loadtxt("dataset/val/labels_val.txt")
+            self.Y = torch.tensor(np.array(self.Y))
             if os.path.exists("dataset/val_X.txt"):
                 self.X = np.loadtxt("dataset/val_X.txt")
                 self.X = np.array(self.X)        #(13821, 28, 28)
                 self.X = self.X.reshape(self.X.shape[0],28,28)
-                return self.X, self.Y
+                self.X = torch.tensor(self.X)
+                return TensorDataset(self.X, self.Y)
             else:
                 path = "dataset/val/images/"
         elif index == 2:
+            #self.Y = torch.tensor(np.array(self.Y))
             if os.path.exists("dataset/test_X.txt"):
                 self.X = np.loadtxt("dataset/test_X.txt")
                 self.X = np.array(self.X)      #(16256, 28, 28)
                 self.X = self.X.reshape(self.X.shape[0],28,28)
-                return self.X, self.Y
+                self.X = torch.tensor(self.X)
+                return TensorDataset(self.X)
             else:
                 path = "dataset/test/images/"
 
@@ -76,7 +84,8 @@ class HandWritingNumberRecognize_Dataset(Dataset):
                 np.savetxt("dataset/val_X.txt",X,fmt="%s")
             elif index == 2:
                 np.savetxt("dataset/test_X.txt",X,fmt="%s")
-            return self.X, self.Y
+            self.X = torch.tensor(self.X)
+            return TensorDataset(self.X, self.Y)
                 
 
     def __len__(self):
@@ -85,24 +94,40 @@ class HandWritingNumberRecognize_Dataset(Dataset):
 
 
 class HandWritingNumberRecognize_Network(torch.nn.Module):
-    def __init__(self,X,Y):
+    def __init__(self):
         super(HandWritingNumberRecognize_Network, self).__init__()
-
-        # Flattening the images from the 28x28 pixels to 1D 784 pixels
-        X = X.reshape(X.shape[0],28,28,1)
-        X = X.astype('float32')
-        X /= 255
-        n_classes = 10
-        Y = np_utils.to_categorical(Y, n_classes)
-        print("Shape after one-hot encoding: ", Y.shape)
-
         # 此处添加网络的相关结构，下面的pass不必保留
-
-        
+        self.conv2d = nn.Conv2d(28, 33, 3, stride=2)
+        self.relu = nn.ReLU()
+        self.maxpool2d = nn.MaxPool2d(2)
+        self.dropout = nn.Dropout(0.25)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(33,100)
+        self.fc2 = nn.Linear(100,10)
+        self.softmax = nn.Softmax()
 
     def forward(self, input_data):
         # 此处添加模型前馈函数的内容，return函数需自行修改
+        input_data = self.conv2d(input_data)
+        input_data = self.relu(input_data)
 
+        '''input_data = self.conv2d(input_data)
+        input_data = self.relu(input_data)
+        input_data = self.maxpool2d(input_data)
+        input_data = self.dropout(input_data)
+        
+        input_data = self.conv2d(input_data)
+        input_data = self.relu(input_data)'''
+        input_data = self.maxpool2d(input_data)
+        input_data = self.dropout(input_data)
+
+        input_data = self.flatten(input_data)
+
+        # hidden layer
+        input_data = self.fc1(input_data)
+        input_data = self.relu(input_data)
+        input_data = self.fc2(input_data)
+        input_data = self.softmax(input_data)
         return input_data
 
 
@@ -132,32 +157,57 @@ def train(epoch_num):
     # 循环外可以自行添加必要内容
     for index, data in enumerate(data_loader_train, 0):
         images, true_labels = data
+
         # 该部分添加训练的主要内容
         # 必要的时候可以添加损失函数值的信息，即训练到现在的平均损失或最后一次的损失，下面两行不必保留
-        print(epoch_num, images, true_labels)
-        pass
+        
+        images, true_labels = preprocess(images, true_labels)
+        y_hat = model(images)   # 模型预测
+        loss = loss_function(y_hat, true_labels)
+        optimizer.zero_grad()   # 梯度清零
+        loss.backward()         # 计算梯度
+        optimizer.step()        # 更新参数
+        y_hat = torch.tensor([torch.argmax(_) for _ in y_hat]).to(device)
+        sum_true += torch.sum(y_hat == true_labels).float()
+        sum_loss += loss.item()
+    
+    train_acc = sum_true / len(dataset_train)
+    train_loss = sum_loss / (len(dataset_train) / 64)    # batch size = 64
+    print(train_acc,train_loss)
 
+
+def preprocess(X,Y):
+    # Flattening the images from the 28x28 pixels to 1D 784 pixels
+    X = X.reshape(X.shape[0],28,28,1)
+    X /= 255
+    n_classes = 10
+    Y = np_utils.to_categorical(Y, n_classes)
+    print("Shape after one-hot encoding: ", Y.shape)
+    return X,Y
 
 if __name__ == "__main__":
 
     # 构建数据集，参数和值需自行查阅相关资料补充。
-    dataset_train = HandWritingNumberRecognize_Dataset("train")
-    dataset_val = HandWritingNumberRecognize_Dataset("val")
-    dataset_test = HandWritingNumberRecognize_Dataset("test")
+    dataset_train = HandWritingNumberRecognize_Dataset("train").dataset
+    dataset_val = HandWritingNumberRecognize_Dataset("val").dataset
+    dataset_test = HandWritingNumberRecognize_Dataset("test").dataset
+   
 
     # 构建数据加载器，参数和值需自行完善。
-    data_loader_train = DataLoader(dataset=dataset_train)
-    data_loader_val = DataLoader(dataset=dataset_val)
-    data_loader_test = DataLoader(dataset=dataset_test)
+    data_loader_train = DataLoader(dataset=dataset_train,batch_size=64,shuffle=True)
+    data_loader_val = DataLoader(dataset=dataset_val,batch_size=64,shuffle=False)
+    data_loader_test = DataLoader(dataset=dataset_test,batch_size=64,shuffle=False)
 
     # 初始化模型对象，可以对其传入相关参数
-    model = HandWritingNumberRecognize_Network(dataset_train.X, dataset_train.Y)
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    print(device)
+    model = HandWritingNumberRecognize_Network().to(device)
 
     # 损失函数设置
-    loss_function = None  # torch.nn中的损失函数进行挑选，并进行参数设置
+    loss_function = nn.CrossEntropyLoss()  # torch.nn中的损失函数进行挑选，并进行参数设置
 
     # 优化器设置
-    optimizer = None  # torch.optim中的优化器进行挑选，并进行参数设置
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-3, weight_decay=5e-4)  # torch.optim中的优化器进行挑选，并进行参数设置
     max_epoch = 1  # 自行设置训练轮数
     num_val = 2  # 经过多少轮进行验证
 
